46741a44ea1e35f4d434b6bfcc14e62a2217bb02
Alfie Edwards <alfie-edwards@hotmail.com>
2026-01-31 12:43:35 +0000

    Spawn player at enterance

--- player.lua
+++ player.lua
@@ -1,7 +1,7 @@
 function init_player()
 	player = {
-		x = 0,
-		y = 0,
+		x = 128,
+		y = 255,
 	}
 end
 
@@ -11,13 +11,13 @@ function update_player()
 			player.x = max(player.x - 1, 0)
 		end
 		if btn(1) then
-			player.x = min(player.x + 1, 256)
+			player.x = min(player.x + 1, 255)
 		end
 		if btn(2) then
 			player.y = max(player.y - 1, 0)
 		end
 		if btn(3) then
-			player.y = min(player.y + 1, 256)
+			player.y = min(player.y + 1, 255)
 		end
 	end
 end
