f1b5107df1ae72d0102281d838bccfa5575ef286
charlie-wt <shout@charlies.computer>
2026-01-31 13:29:24 +0000

    separate sprites for player direction

--- player.lua
+++ player.lua
@@ -1,27 +1,98 @@
+direction = {
+	up = "up",
+	down = "down",
+	left = "left",
+	right = "right",
+}
+
+input_map = {
+	[0] = direction.left,
+	[1] = direction.right,
+	[2] = direction.up,
+	[3] = direction.down
+}
+
 function init_player()
 	player = {
 		x = 128,
 		y = 255,
+		max_speed = 1,
+		dir = direction.down,
+		last_started_pressing = {
+			up = 0,
+			down = 0,
+			left = 0,
+			right = 0,
+		},
+		last_released = direction.down,
 	}
+end
+
+function move(dir)
+	local offset = { x=0, y=0 }
+	if dir == direction.up then
+		offset.y = -player.max_speed
+	elseif dir == direction.down then
+		offset.y = player.max_speed
+	elseif dir == direction.left then
+		offset.x = -player.max_speed
+	elseif dir == direction.right then
+		offset.x = player.max_speed
+	else
+		assert(false == "unexpected direction "..dir)
+	end
+
+	if player.last_started_pressing[dir] == 0 then
+		player.last_started_pressing[dir] = time()
+	end
+
+	player.x = min(max(player.x + offset.x, 0), 255)
+	player.y = min(max(player.y + offset.y, 0), 255)
+end
+
+function stop_moving(dir)
+	if player.last_started_pressing[dir] ~= 0 then
+		player.last_released = dir
+	end
+	player.last_started_pressing[dir] = 0
 end
 
 function update_player()
 	if not saying then
-		if btn(0) then
-			player.x = max(player.x - 1, 0)
+		for b,d in pairs(input_map) do
+			if btn(b) then
+				move(d)
+			else
+				stop_moving(d)
+			end
 		end
-		if btn(1) then
-			player.x = min(player.x + 1, 255)
+
+		local visual_direction = player.last_released
+		for d,t in pairs(player.last_started_pressing) do
+			if t > player.last_started_pressing[visual_direction] then
+				visual_direction = d
+			end
 		end
-		if btn(2) then
-			player.y = max(player.y - 1, 0)
-		end
-		if btn(3) then
-			player.y = min(player.y + 1, 255)
-		end
+		player.dir = visual_direction
 	end
 end
 
 function draw_player()
-	draw_sprite(sprites.player, player.x, player.y)
+	local flip = false
+	local sprite = nil
+	if player.dir == direction.up then
+		sprite = sprites.player_back
+	elseif player.dir == direction.down then
+		sprite = sprites.player_front
+	elseif player.dir == direction.left then
+		sprite = sprites.player_side
+	elseif player.dir == direction.right then
+		sprite = sprites.player_side
+		flip = true
+	else
+		assert(false == "unexpected direction "..player.dir)
+	end
+	assert(sprite ~= nil)
+
+	draw_sprite(sprite, player.x, player.y, flip)
 end
