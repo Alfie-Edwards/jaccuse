5adfa59d4c3e3e0a982072ca5debacfd6be6ea7a
charlie-wt <shout@charlies.computer>
2026-01-31 11:38:58 +0000

    guests can cycle through multiple bits of dialogue

--- scenario.lua
+++ scenario.lua
@@ -6,55 +6,58 @@ function init_scenario()
 				x = 10,
 				y = 200,
 				sprite = sprites.guest1,
-				dialogue = "Hello my name is a",
+				dialogue = {"Hello my name is a", "I like eating toast"},
 			},
 			{
 				x = 10,
 				y = 10,
 				sprite = sprites.guest2,
-				dialogue = "Hello my name is b",
+				dialogue = {"Hello my name is b", "This week I have been mostly eating taramasalata"},
 			},
 			{
 				x = 50,
 				y = 15,
 				sprite = sprites.guest3,
-				dialogue = "Hello my name is c",
+				dialogue = {"Hello my name is c"},
 			},
 			{
 				x = 80,
 				y = 12,
 				sprite = sprites.guest4,
-				dialogue = "Hello my name is d",
+				dialogue = {"Hello my name is d"},
 			},
 			{
 				x = 200,
 				y = 200,
 				sprite = sprites.guest5,
-				dialogue = "Hello my name is e",
+				dialogue = {"Hello my name is e"},
 			},
 			{
 				x = 100,
 				y = 230,
 				sprite = sprites.guest6,
-				dialogue = "Hello my name is f",
+				dialogue = {"Hello my name is f"},
 			},
 			{
 				x = 120,
 				y = 230,
 				sprite = sprites.guest7,
-				dialogue = "Hello my name is g",
+				dialogue = {"Hello my name is g"},
 			},
 			{
 				x = 70,
 				y = 90,
 				sprite = sprites.guest8,
-				dialogue = "Hello my name is h",
+				dialogue = {"Hello my name is h"},
 			}
 		}
 	}
 
 	scenario = main
 	interaction = nil
+	for _,g in ipairs(main.guests) do
+		g.next_dialogue_idx = 0
+	end
 end
 
 
@@ -80,12 +83,22 @@ function get_interaction()
 end
 
 
+-- talk to a guest (defaulting to current `interaction`)
+function talk_to(guest)
+	if guest == nil then guest = interaction end
+	assert(guest ~= nil)
+	assert(guest.next_dialogue_idx ~= nil)
+
+	say(guest.dialogue[(guest.next_dialogue_idx % #guest.dialogue) + 1])
+	guest.next_dialogue_idx += 1
+end
+
+
 function update_interaction()
 	interaction = get_interaction()
-	printh(interaction)
 
 	if interaction and btnp(4) then
-		say(interaction.dialogue)
+		talk_to()
 		interaction = nil
 	end
 end
