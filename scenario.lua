5f8af03feb0fe8adaf86d8ebfa706c300fa2f8b2
Alfie Edwards <alfie-edwards@hotmail.com>
2026-01-31 00:38:20 +0000

    Add basic scenario

---
+++ scenario.lua
@@ -0,0 +1,63 @@
+main = {
+	guilty = "a",
+	characters = {
+		{
+			x = 10,
+			y = 200,
+			sprite = 0,
+			dialogue = "Hello my name is a",
+		},
+		{
+			x = 10,
+			y = 10,
+			sprite = 2,
+			dialogue = "Hello my name is b",
+		},
+		{
+			x = 50,
+			y = 15,
+			sprite = 4,
+			dialogue = "Hello my name is c",
+		},
+		{
+			x = 80,
+			y = 12,
+			sprite = 6,
+			dialogue = "Hello my name is d",
+		},
+		{
+			x = 200,
+			y = 200,
+			sprite = 8,
+			dialogue = "Hello my name is e",
+		},
+		{
+			x = 100,
+			y = 230,
+			sprite = 10,
+			dialogue = "Hello my name is f",
+		},
+		{
+			x = 120,
+			y = 230,
+			sprite = 12,
+			dialogue = "Hello my name is g",
+		},
+		{
+			x = 70,
+			y = 90,
+			sprite = 14,
+			dialogue = "Hello my name is h",
+		}
+	}
+}
+
+function init_scenario()
+	scenario = main
+end
+
+function draw_characters()
+	for _, character in ipairs(scenario.characters) do
+		spr(character.sprite, character.x, character.y, 2, 4)
+	end
+end
