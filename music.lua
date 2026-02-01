357044461a960edfe93618ce0cc99ec4d91d4fe7
cc <>
2026-02-01 12:21:57 +0000

    Add won and lost music

--- music.lua
+++ music.lua
@@ -1,10 +1,18 @@
 function init_music()
-	bg_music_started = false
+	music_state = "intro"
 end
 
 function update_music()
-	if not bg_music_started and scene == "game" then
+	if music_state == "intro" and scene == "game" then
 		music(0)
-		bg_music_started = true
+		music_state = "bg_music"
+	elseif music_state ~= "won" and won() then
+		music(-1)
+		music(24)
+		music_state = "won"
+	elseif music_state ~= "lost" and lost() then
+		music(-1)
+		music(32)
+		music_state = "lost"
 	end
 end
